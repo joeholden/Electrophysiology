25d18e9bff54aee3943d6ec9eacc92967e69ad3b
Joe Holden <joemattholden@gmail.com>
2023-07-28 11:30:44 -0500

    Add files via upload
    added IV curve function to tools.py

--- tools.py
+++ tools.py
@@ -3,12 +3,18 @@ import matplotlib.pyplot as plt
 from pathlib import Path
 from scipy.signal import savgol_filter
 import numpy as np
+import statistics as stats
+import pandas as pd
+import os
+
 
 path = "C:/Users/Acer/Desktop/2023-07-26 great patch day/ABF/23726020.abf"
 
 
 def show_trace(abf_file_path, trace_number=0, color='#7852A9', channel=1, smooth=0, x_override=None):
     """
+    Plots a single trace of an .abf file
+    
     :param abf_file_path: path to single abf file
     :param trace_number: which sweep do you want to plot
     :param color: accepts hex or written color for plot trace. Default is purple
@@ -69,6 +75,8 @@ def show_trace(abf_file_path, trace_number=0, color='#7852A9', channel=1, smooth
 
 def show_traces(abf_file_path, channel=1, smooth=0, x_override=None):
     """
+    Plots All traces of an .abf file
+
     :param abf_file_path: path to single abf file
     :param channel: which channel is to be plotted? 0 or 1
     :param smooth: Do you want to smooth the plot? 0 No, 1 Yes, 2 Yes and display smoothed and un-smoothed data].
@@ -131,4 +139,88 @@ def show_traces(abf_file_path, channel=1, smooth=0, x_override=None):
     plt.show()
 
 
-show_traces(path, smooth=2)
+def iv_curve(excel_save_path, png_save_path, abf_path, mean_or_abs_max='mean', save_fig=True, plot_linear=False,
+             save_data=True, print_data=False):
+    """
+    Plots current voltage diagram for a single abf file
+
+    :param excel_save_path: Path to directory you want to save IV plot Excel data
+    :param png_save_path: Path to directory you want to save IV plot images
+    :param abf_path: Path to single abf file
+    :param mean_or_abs_max: Do you want the IV curve to use the mean current in the stimulation range
+            or the absolute value max? 'mean' or 'max'
+    :param save_fig: Boolean
+    :param plot_linear: Boolean value if you want to plot a linear line on the plot for rectification reference
+    :param save_data: Boolean to save Excel data points
+    :param print_data: Boolean to display the Excel data points in console
+    :return:
+    """
+
+    abf = pyabf.ABF(abf_path)
+    colors = plt.cm.Spectral(np.linspace(0.4, 1, abf.sweepCount))
+    voltages = [-95 + 10 * i for i in range(14)]
+    currents = []
+    max_currents = []
+
+    # Get current values for each sweep
+    for sweep in range(abf.sweepCount):
+        try:
+            abf.setSweep(sweep, channel=1)
+        except ValueError:
+            abf.setSweep(sweep, channel=0)
+
+        mid_voltage = abf.sweepY[round(50000 * .08)]
+        mean_voltage = stats.mean(abf.sweepY[(round(50000 * .06)):round(50000 * .10)])
+        min_voltage = min(abf.sweepY[(round(50000 * .06)):round(50000 * .10)])
+        max_voltage = max(abf.sweepY[(round(50000 * .06)):round(50000 * .10)])
+
+        if abs(min_voltage) > abs(max_voltage):
+            max_abs = min_voltage
+        else:
+            max_abs = max_voltage
+
+        currents.append(mean_voltage)
+        max_currents.append(max_abs)
+
+    # Plot IV curve
+    fig = plt.figure(figsize=(9, 6))
+    plt.style.use('ggplot')
+    if mean_or_abs_max == 'mean':
+        plt.scatter(voltages, currents, color='#7852A9')
+        plt.plot(voltages, currents, color='#7852A9', label='IV Plot')
+    elif mean_or_abs_max == 'max':
+        plt.scatter(voltages, max_currents, color='#7852A9')
+        plt.plot(voltages, max_currents, color='#7852A9', label='IV Plot')
+    else:
+        print('Incorrect Spelling of "mean" or "max"')
+        raise ValueError
+
+    if plot_linear:
+        plt.plot(voltages, voltages, color='#eb9605', label='Linear')
+
+    # Show Zero lines
+    plt.axvline(0, color='black', linestyle="--")
+    plt.axhline(0, color='black', linestyle="--")
+
+    # Format Plot
+    plt.title(f"Current Voltage Diagram\n{Path(abf_path).stem}", fontsize=17)
+    plt.ylabel('Current (pA)', fontsize=15)
+    plt.xlabel('Voltage (mV)', fontsize=15)
+    if save_fig:
+        plt.savefig(os.path.join(png_save_path, f'{abf.abfID} _ {abf.protocol}.png'))
+    plt.show()
+
+    if save_data:
+        data = pd.DataFrame(pd.Series(voltages))
+        data = pd.concat([data, pd.Series(currents)], axis=1)
+        data.columns = ['Voltage', 'Current']
+        data.to_excel(os.path.join(excel_save_path, f"{abf.abfID}.xlsx"))
+        if print_data:
+            print(data.head)
+
+
+iv_curve(plot_linear=True,
+         excel_save_path="C:/Users/Acer/Desktop/patching astrocytes/Excel IV plots/",
+         png_save_path="C:/Users/Acer/Desktop/patching astrocytes/IV PNG/",
+         abf_path="C:/Users/Acer/Desktop/2023-07-20/ABF/23720012.abf",
+         )
